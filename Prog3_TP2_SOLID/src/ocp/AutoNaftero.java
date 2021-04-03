361efa82304071115e2ec300b0ba5e47e2225ebb
NicoChanampe <49734310+NicoChanampe@users.noreply.github.com>
2021-04-03 20:40:59 -0300

    [TP2_1]Creación de ejemplo con SRP (Triangulo)

---
+++ Prog3_TP2_SOLID/src/ocp/AutoNaftero.java
@@ -0,0 +1,16 @@
+package ocp;
+
+public class AutoNaftero extends Automovil{
+
+    public AutoNaftero(String marca, String modelo) {
+        super(marca, modelo);
+    }
+
+    public void calibrar() {
+        super.calibrarNeumaticos();
+    }
+
+    public String recargarCombustible() {
+        return "Cargar Nafta";
+    }
+}
